16ee0476ab1f7224bf090850af058222a91a8c78
Vitor Mendonca <vitor.mendonca95@gmail.com>
2020-08-28 15:47:00 -0300

    Adicionar arquivos de projeto.

---
+++ WorkerSalaryCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkerSalaryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
